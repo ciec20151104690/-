185837a9b3e06c3bd319f4acc6e3ff3e064d006c
人啊！！！ <s20151104690@1.local>
2017-02-27 12:00:33 +0800

    叠加程序
    1+1加100次，尚且未完成

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  叠加程序
+//
+//  Created by 20151104690 on 17/2/27.
+//  Copyright © 2017年 4690. All rights reserved.
+//
+
+import Foundation
+let base=1
+
+var answer = 1
+for _ in 1...100{
+    answer=answer+base
+}
+print("\(answer)")
